1848151774ec8ee216478275d3f73b7488e5bee0
Kyb3r Cipher <me@kyb3rcipher.com>
2025-04-06 09:01:02 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'drop-market',
+  webDir: 'www'
+};
+
+export default config;
