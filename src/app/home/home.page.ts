ea643164a22347fa3f9ccaca3bb3c96fd8c26fff
Kyb3r Cipher <me@kyb3rcipher.com>
2025-05-29 23:16:44 -0600

    Add home page top sales section

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,70 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
+  topSales = [
+    {
+      title: 'Gorra Barbas Hats 77 Total Black',
+      image: 'https://drop-shop.mx/cdn/shop/files/99D26807-73B1-4E47-910B-F6E92885EAFD.jpg?v=1740584451&width=533',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'GORRA 31 HATS NY PEARLS IN LOVE',
+      image: 'https://drop-shop.mx/cdn/shop/files/IMG-0803.jpg',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'Gorra Barbas Hats 77 Total Black',
+      image: 'https://drop-shop.mx/cdn/shop/files/99D26807-73B1-4E47-910B-F6E92885EAFD.jpg?v=1740584451&width=533',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'GORRA 31 HATS NY PEARLS IN LOVE',
+      image: 'https://drop-shop.mx/cdn/shop/files/IMG-0803.jpg',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'Gorra Barbas Hats 77 Total Black',
+      image: 'https://drop-shop.mx/cdn/shop/files/99D26807-73B1-4E47-910B-F6E92885EAFD.jpg?v=1740584451&width=533',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'GORRA 31 HATS NY PEARLS IN LOVE',
+      image: 'https://drop-shop.mx/cdn/shop/files/IMG-0803.jpg',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'Gorra Barbas Hats 77 Total Black',
+      image: 'https://drop-shop.mx/cdn/shop/files/99D26807-73B1-4E47-910B-F6E92885EAFD.jpg?v=1740584451&width=533',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'GORRA 31 HATS NY PEARLS IN LOVE',
+      image: 'https://drop-shop.mx/cdn/shop/files/IMG-0803.jpg',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'Gorra Barbas Hats 77 Total Black',
+      image: 'https://drop-shop.mx/cdn/shop/files/99D26807-73B1-4E47-910B-F6E92885EAFD.jpg?v=1740584451&width=533',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    },
+    {
+      title: 'GORRA 31 HATS NY PEARLS IN LOVE',
+      image: 'https://drop-shop.mx/cdn/shop/files/IMG-0803.jpg',
+      price: '$ 3,100.00',
+      link: '/products/gorra-barbas-hats-77-total-black-preventa'
+    }
+  ];  
+  
   constructor() {}
 
+    
 }
