9e7b66ac5c9d0c864a6c5891bc2be792cdc47052
Kyb3r Cipher <me@kyb3rcipher.com>
2025-05-31 11:02:31 -0600

    FInish home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -70,6 +70,56 @@ export class HomePage {
     }
   ];  
   
+
+  products = [
+    {
+      image: 'https://drop-shop.mx/cdn/shop/files/s-l1600_db107192-7113-4af4-b484-1b616fd55c9e.webp?v=1730748842',
+      title: 'Labubu "The monsters" Exciting Macaron',
+      price: '1,200'
+    },
+    {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "The monsters" Have a Seat',
+      price: '999'
+    },
+    {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    },
+    {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    },
+    {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    }, {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    }, {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    }, {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    }, {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    }, {
+      image: 'https://drop-shop.mx/cdn/shop/files/image_67_48f5bae1-afc1-4563-9159-1ca06c211cd1.webp',
+      title: 'Labubu "Fall in wild"',
+      price: '999'
+    },
+  ];
+
+  
   constructor() {}
 
     
